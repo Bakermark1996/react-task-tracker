85692c5a640c2c3f094bd5990f54b15a986418dc
Bakermark1996 <bakermark1996@gmail.com>
2023-01-31 16:00:27 -0800

    Initital Commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h2>Created By: Mark Baker</h2>
+      <h4>Version 1.0.0</h4>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
